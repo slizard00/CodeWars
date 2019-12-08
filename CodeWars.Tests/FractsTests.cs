c00a090c309906c8efc7062cf9685596f2c0e40d
slizard00 <slizard00@users.noreply.github.com>
2019-12-08 02:01:04 -0600

    first kata and test

---
+++ CodeWars.Tests/FractsTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+
+namespace CodeWars.Tests
+{
+
+
+    [TestFixture]
+    public class FractsTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+
+            long[,] lst = new long[,] { { 1, 2 }, { 1, 3 }, { 1, 4 } };
+            Assert.AreEqual("(6,12)(4,12)(3,12)", Fracts.convertFrac(lst));
+
+        }
+    }
+}
---
+++ Katas/Fracts.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class Fracts
+    {
+        public static string convertFrac(long[,] lst)
+        {
+            var denominators = new List<long>();
+            for (int i = 0; i < lst.Length / 2; i++)
+            {
+                var botton = lst[i, 1];
+                denominators.Add(botton);
+            }
+
+            long gcd = 1;
+            denominators.Distinct().ToList().ForEach(x => gcd *= x);
+
+            long[,] newList = (long[,])lst.Clone();
+            for (int i = 0; i < lst.Length / 2; i++)
+            {
+                var denominator = lst[i, 1];
+                var v = gcd / denominator;
+                var top = lst[i, 0];
+                var newtop = top * v;
+                newList[i, 0] = newtop;
+                newList[i, 1] = gcd;
+            }
+
+            long log = 1;
+            for (int i = 2; i < gcd / 2; i++)
+            {
+                var eee = newList.Cast<long>().ToArray();
+                if (eee.All(x => (x % i) == 0))
+                {
+                    log = i;
+                    break;
+                }
+            }
+            for (int i = 0; i < newList.Length / 2; i++)
+            {
+                newList[i, 0] = newList[i, 0] / log;
+                newList[i, 1] = newList[i, 1] / log;
+            }
+
+            var output = string.Empty;
+            for (int i = 0; i < newList.Length / 2; i++)
+            {
+                output += $"({newList[i, 0]},{newList[i, 1]})";
+            }
+            return output;
+        }
+    }
+}
