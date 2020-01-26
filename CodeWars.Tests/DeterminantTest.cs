565122f4d721b6d7c168a93e42df466fcf966580
slizard00 <slizard00@users.noreply.github.com>
2020-01-25 20:37:31 -0600

    adding determint code

---
+++ CodeWars.Tests/DeterminantTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Katas;
+
+namespace CodeWars.Tests
+{
+
+
+    [TestFixture]
+    class DeterminantTest
+    {
+        private static int[][][] matrix =
+{
+        new int[][] { new [] { 13 } },
+
+        new int[][] {   new [] { 1, 3 },
+                        new [] { 2, 5 } },
+
+        new int[][] {   new [] { 2, 5, 3 },
+                        new [] { 1, -2, -1 }, 
+                        new [] { 1, 3, 4 } },
+
+        new int[][] {   new [] { 2, 5, 3, 1 },
+                        new [] { 1, -2, -1, 1 },
+                        new [] { 1, 3, 4, 1 },
+                        new [] { 1, 2, 3, 4 } },
+
+        new int[][] { new [] { 2,  5,  3,  1,  6 }, 
+                      new [] { 1, -2, -1, 11,  9 }, 
+                      new [] { 1,  3,  4,  1,  3 }, 
+                      new [] { 1,  2,  3,  4, -8 },
+                      new [] { 9, -2,  7, -7, -5 }, },
+
+        //new int[][] { new [] {  2,   5,   3, 12,  6, 32 },
+        //              new [] { 20,  -2, -19, 11,  9, -31 },
+        //              new [] { 19,   3,   4,  3, 35, -7 },
+        //              new [] { 77,   2,  33,  4, -8, 23 },
+        //              new [] {  9,  -2,   7, 37, -5, 14 },
+        //              new [] { -91, 42,  32, -7, -5, 3 }, },
+        };
+        //3918523548
+       
+        private static int[] expected = { 13, -1, -20, -60, 19392 };
+
+        private static string[] msg = { 
+            "Determinant of a 1 x 1 matrix yields the value of the one element", 
+            "Should return 1 * 5 - 3 * 2 == -1 ", 
+            "", 
+            "",
+            "",
+            ""};
+
+        [Test]
+        public void SampleTests()
+        {
+            for (int n = 0; n < expected.Length; n++)
+                Assert.AreEqual(expected[n],Matrix.Determinant(matrix[n]), msg[n]);
+        }
+
+
+    }
+}
---
+++ Katas/Determinant.cs
@@ -0,0 +1,66 @@
+﻿namespace Katas
+{
+    public class Matrix
+    {
+        public static int Determinant(int[][] matrix)
+        {
+            var length = matrix.GetLength(0);
+            if (length == 1) return matrix[0][0];
+            if (length == 2) return Calculate2x2Matrix(matrix);
+            return CalculateBiggerMatrix(matrix);
+        }
+
+        private static int CalculateBiggerMatrix(int[][] matrix)
+        {
+            var add = true;
+            var total = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var determinant = CalculateDeterminant(i, matrix);
+                total += add ? determinant : determinant * -1;
+                add = !add;
+            }
+            return total;
+        }
+
+        private static int CalculateDeterminant(int a_index, int[][] matrix)
+        {
+            int a_value = matrix[0][a_index];
+            return a_value * Determinant(GetSmallerMatrix(a_index, matrix));
+        }
+
+        private static int Calculate2x2Matrix(int[][] matrix)
+        {
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        }
+
+        private static int[][] GetSmallerMatrix(int a_index, int[][] matrix)
+        {
+            //Create a new matrix that is 1 row and column smaller than the original matrix
+            int[][] smallerMatrix = new int[matrix.GetLength(0) - 1][];
+
+            for (int r = 0; r < matrix.GetLength(0) - 1; r++)
+            {
+                smallerMatrix[r] = new int[matrix.GetLength(0) - 1];
+
+                for (int c = 0; c < matrix.GetLength(0); c++)
+                {
+                    if (c < a_index)
+                    {
+                        smallerMatrix[r][c] = matrix[r + 1][c];
+                    }
+                    else if (c == a_index)
+                    {
+                        //skip this column...
+                    }
+                    else
+                    {
+                        smallerMatrix[r][c - 1] = matrix[r + 1][c];
+                    }
+                }
+            }
+
+            return smallerMatrix;
+        }
+    }
+}
