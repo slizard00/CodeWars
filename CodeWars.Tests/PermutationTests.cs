5662def04125bf13d7dfd3479341cf6e3835acb6
slizard00 <slizard00@users.noreply.github.com>
2020-01-25 22:21:24 -0600

    added code for permutations kata

---
+++ CodeWars.Tests/PermutationTests.cs
@@ -0,0 +1,35 @@
+﻿using Katas;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.Tests
+{
+    class PermutationTests
+    {
+
+        [Test]
+         public void Example1()
+        {
+            Assert.AreEqual(new List<string> { "a" }, Permutations.SinglePermutations("a").OrderBy(x => x).ToList());
+        }
+
+        [Test]
+        public void Example2()
+        {
+            Assert.AreEqual(new List<string> { "ab", "ba" }, Permutations.SinglePermutations("ab").OrderBy(x => x).ToList());
+        }
+
+        [Test]
+        public void Example3()
+        {
+            Assert.AreEqual(new List<string> { "aabb", "abab", "abba", "baab", "baba", "bbaa" }, Permutations.SinglePermutations("aabb").OrderBy(x => x).ToList());
+        }
+
+        [Test]
+        public void Example4()
+        {
+            Assert.AreEqual(new List<string> { "abc", "acb", "bac", "bca", "cab", "cba" }, Permutations.SinglePermutations("abc").OrderBy(x => x).ToList());
+        }
+    }
+}
---
+++ Katas/Permutations.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Katas
+{
+    public class Permutations
+    {
+        public static List<string> SinglePermutations(string s)
+        {
+            var allCombos = BuildCombinationsRecursive("", s);
+            return allCombos.Select(sb => sb.ToString()).Distinct().OrderBy(x => x).ToList();
+        }
+        
+        private static List<StringBuilder> BuildCombinationsRecursive(string baseString, string remainingChars)
+        {
+            var sbList = new List<StringBuilder>();
+
+            for (int i = 0; i <= remainingChars.Length - 1; i++)
+            {
+                var newBaseString = new StringBuilder(baseString);
+                newBaseString.Append(remainingChars[i]);
+
+                if (remainingChars.Length == 1)
+                {
+                    sbList.Add(newBaseString);
+                    return sbList;
+                }
+                
+                var sublist = BuildCombinationsRecursive(newBaseString.ToString(), GetNewRemainingCharacters(remainingChars, i));
+                foreach (var item in sublist)
+                    sbList.Add(item);
+            }
+            return sbList;
+        }
+
+        private static string GetNewRemainingCharacters(string remainingChars, int i)
+        {
+            var newRemainingChars = new StringBuilder();
+            for (int a = 0; a <= remainingChars.Length - 1; a++)
+                if (a != i) newRemainingChars.Append(remainingChars[a]);
+            return newRemainingChars.ToString();
+        }
+    }
+}
