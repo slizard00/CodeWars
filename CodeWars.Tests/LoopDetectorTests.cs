57c19468f8ae27f22a147edee7f4d1d9f03f5ceb
slizard00 <slizard00@users.noreply.github.com>
2020-01-26 20:56:22 -0600

    initial solution for finding loop in a chain

---
+++ CodeWars.Tests/LoopDetectorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    public class LoopDetectorTests
+    {
+        [Test]
+        public void FourNodesWithLoopSize3()
+        {
+            var n1 = new LoopDetector.Node();
+            var n2 = new LoopDetector.Node();
+            var n3 = new LoopDetector.Node();
+            var n4 = new LoopDetector.Node();
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n4;
+            n4.next = n2;
+            Assert.AreEqual(3, Kata.getLoopSize(n1));
+        }
+
+        [Test]
+        public void RandomChainNodesWithLoopSize30()
+        {
+            var n1 = LoopDetector.createChain(3, 30);
+            Assert.AreEqual(30, Kata.getLoopSize(n1));
+        }
+
+        [Test]
+        public void RandomLongChainNodesWithLoopSize1087()
+        {
+            var n1 = LoopDetector.createChain(3904, 1087);
+            Assert.AreEqual(1087, Kata.getLoopSize(n1));
+        }
+    }
+}
---
+++ Katas/LoopDetector.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Kata
+{
+    public static int getLoopSize(LoopDetector.Node startNode)
+    {
+        var list = new List<LoopDetector.Node> { startNode }; 
+        var currentNode = startNode.next;
+        var duplicateFound = false;
+        do
+        {
+            if (list.Contains(currentNode))
+                duplicateFound = true;
+            else
+            {
+                list.Add(currentNode);
+                currentNode = currentNode.next;
+            }        
+        } while (!duplicateFound);
+
+        var lengthOfTail = list.Count;
+        foreach (var item in list)
+        {
+            if (item != currentNode)
+                lengthOfTail--;
+            else
+                break;
+        }
+
+        return lengthOfTail;
+    }
+}
+
+public class LoopDetector
+{
+    public static Node createChain(int tailLength, int loopLength)
+    {
+        var endOfTailNode = new Node();
+        var currentNode = endOfTailNode;
+        for (int i = 1; i <= tailLength; i++)
+        {
+            currentNode.next = new Node();
+            currentNode = currentNode.next;
+        }
+
+        var firstNodeOfLoop = new Node();
+        currentNode.next = firstNodeOfLoop;
+        firstNodeOfLoop.next = new Node();
+        currentNode = firstNodeOfLoop.next;
+        for (int i = 2; i <= loopLength; i++)
+        {
+            if (i == loopLength)
+            {
+                currentNode.next = firstNodeOfLoop;
+            }
+            else
+            {
+                currentNode.next = new Node();
+                currentNode = currentNode.next;
+            }
+        }
+        return endOfTailNode;
+    }
+    public class Node
+    {
+        public Node next { get; set; }
+    }
+}
+
+    
