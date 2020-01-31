2ed2ec5a7e4cec5d3f4e47c4813f1288457e985b
slizard00 <slizard00@users.noreply.github.com>
2020-01-30 18:07:23 -0600

    adding base64Util and tests

---
+++ CodeWars.Tests/Base64UtilsTest.cs
@@ -0,0 +1,28 @@
+﻿using Katas;
+using NUnit.Framework;
+
+
+namespace CodeWars.Tests
+{
+    [TestFixture]
+    public class Base64UtilsTest
+    {
+        [TestCase("this is a string!!", "dGhpcyBpcyBhIHN0cmluZyEh")]
+        [TestCase("ee", "ZWU=")]
+        [TestCase("e", "ZQ==")]
+        [TestCase("", "")]
+        public void SampleValueEncodeTest(string value, string expected)
+        {
+            Assert.AreEqual(expected, Base64Utils.ToBase64(value));
+        }
+
+        [TestCase("dGhpcyBpcyBhIHN0cmluZyEh", "this is a string!!")]
+        [TestCase("ZWU=", "ee")]
+        [TestCase("ZQ==", "e")]
+        [TestCase("", "")]
+        public void SampleValueDecodeTest(string value, string expected)
+        {
+            Assert.AreEqual(expected, Base64Utils.FromBase64(value));
+        }
+    }
+}
---
+++ Katas/Base64Utils.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Katas
+{
+    public static class Base64Utils
+    {
+        public static string ToBase64(string s)
+        {
+            var bytes = Encoding.ASCII.GetBytes(s);
+            return System.Convert.ToBase64String(bytes);
+        }
+
+        public static string FromBase64(string s)
+        {
+            var bytes = System.Convert.FromBase64String(s);
+            return Encoding.ASCII.GetString(bytes);
+        }
+    }
+}
